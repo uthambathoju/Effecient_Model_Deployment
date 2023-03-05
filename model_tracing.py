594a22cd80d5d62258223e92f18f9ee3fafb4866
Utham Bathoju <uthamkanth@gmail.com>
2023-03-05 22:40:59 +0530

    Create model_tracing.py

---
+++ model_tracing.py
@@ -0,0 +1,73 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+import numpy as np
+from PIL import Image
+
+
+class ConvNet(nn.Module):
+    def __init__(self):
+        super(ConvNet, self).__init__()
+        self.cn1 = nn.Conv2d(1, 16, 3, 1)
+        self.cn2 = nn.Conv2d(16, 32, 3, 1)
+        self.dp1 = nn.Dropout2d(0.10)
+        self.dp2 = nn.Dropout2d(0.25)
+        self.fc1 = nn.Linear(4608, 64) # 4608 is basically 12 X 12 X 32
+        self.fc2 = nn.Linear(64, 10)
+ 
+    def forward(self, x):
+        x = self.cn1(x)
+        x = F.relu(x)
+        x = self.cn2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dp1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dp2(x)
+        x = self.fc2(x)
+        op = F.log_softmax(x, dim=1)
+        return op
+    
+model = ConvNet()
+
+
+PATH_TO_MODEL = "./convnet.pth"
+model.load_state_dict(torch.load(PATH_TO_MODEL, map_location="cpu"))
+
+
+model.eval()
+for p in model.parameters():
+    p.requires_grad_(False)
+    
+    
+demo_input = torch.ones(1, 1, 28, 28)
+traced_model = torch.jit.trace(model, demo_input)
+
+print(traced_model.code)
+
+
+torch.jit.save(traced_model, 'traced_convnet.pt')
+
+loaded_traced_model = torch.jit.load('traced_convnet.pt')
+
+image = Image.open("./digit_image.jpg")
+
+def image_to_tensor(image):
+    gray_image = transforms.functional.to_grayscale(image)
+    resized_image = transforms.functional.resize(gray_image, (28, 28))
+    input_image_tensor = transforms.functional.to_tensor(resized_image)
+    input_image_tensor_norm = transforms.functional.normalize(input_image_tensor, (0.1302,), (0.3069,))
+    return input_image_tensor_norm
+
+input_tensor = image_to_tensor(image)
+
+loaded_traced_model(input_tensor.unsqueeze(0))
+
+model(input_tensor.unsqueeze(0))
+
